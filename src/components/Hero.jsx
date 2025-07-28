25d8b664d41900d1fd6d3b02fe623b62cc0ccfda
umar24nov <umar24nov@gmail.com>
2025-07-29 00:05:42 +0530

    Change in Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,16 @@
-import heroBg from '../assets/wallpaperfigma.jpg'; // Correct import path
+import heroBg from '../assets/wallpaperfigma.jpg';
+
 export default function Hero() {
   return (
-    <section className="bg-gray-100 py-30 px-2 text-center bg-cover bg-center w-screen h-[950px]   md:h:full" style={{ backgroundImage: `url(${heroBg})` }}>
-      <h2 className="text-3xl md:text-5xl font-bold mb-4 text-center mt-40 text-white">
+    <section
+      className="w-screen h-screen md:h-[950px] bg-cover bg-center bg-no-repeat bg-gray-100 px-2 py-10 text-center"
+      style={{ backgroundImage: `url(${heroBg})` }}
+    >
+      <h2 className="text-3xl md:text-5xl font-bold mb-4 text-white mt-40">
         Transform Your Space with <br />
         Timeless Elegance
       </h2>
-      <p className="max-w-xl mx-auto text-white mb-6  text-xl">
+      <p className="max-w-xl mx-auto text-white mb-6 text-xl">
         Discover handcrafted, modern furniture designed to elevate your home.
         Built with precision. Styled for luxury. Made to last.
       </p>
